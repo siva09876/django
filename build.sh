6c20d80c35c21a540a1d5fe9856c5c8f13ec49be
Siva <120114929+siva09876@users.noreply.github.com>
2023-01-17 08:13:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
+
 echo "Make Migrations..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+
+
+gunicorn student.wsgi:application --bind 0.0.0.0:$PORT
